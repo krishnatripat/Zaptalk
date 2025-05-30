d81e9e95b9705de7c85bffe677cb6d4b5936e18c
krishnatripat <krishnatripathi2024@gmail.com>
2025-05-30 19:20:27 +0530

    Fix next  page

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST="http://localhost:3001";
+export const HOST="https://zaptalk-2.onrender.com";
 const AUTH_ROUTE=`${HOST}/api/auth`;
 const MESSAGES_ROUTE=`${HOST}/api/messages`;
 export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
