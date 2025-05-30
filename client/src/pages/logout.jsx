63e07ae085e5ed55a338664da41771bd45ca17dc
krishnatripat <krishnatripathi2024@gmail.com>
2025-05-30 18:07:05 +0530

    Fix logout page

--- client/src/pages/logout.jsx
+++ client/src/pages/logout.jsx
@@ -5,16 +5,20 @@ import { signOut } from "firebase/auth";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
-function logout() {
-  const [{ socket, userInfo }, dispatch] = useStateProvider()
-  const router = useRouter()
+function Logout() {
+  const [{ socket, userInfo }, dispatch] = useStateProvider();
+  const router = useRouter();
+
   useEffect(() => {
-    socket.current.emit("signout",userInfo.import {  } from 'module')
-    dispatch({ type: reducerCases.SET_USER_INFO, userInfo: undefined })
-    signOut(firebaseAuth)
-    router.push("login")
-  }, [socket])
+    if (socket?.current) {
+      socket.current.emit("signout", userInfo);
+    }
+    dispatch({ type: reducerCases.SET_USER_INFO, userInfo: undefined });
+    signOut(firebaseAuth);
+    router.push("login");
+  }, [socket]);
+
   return <div className="bg-conversation-panel-background"></div>;
 }
 
-export default logout;
+export default Logout;
