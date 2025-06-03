12611400e178d7bac7233b862bbd4fa1346b0c47
krishna tripathi <154440810+krishnatripat@users.noreply.github.com>
2025-06-03 19:35:40 +0530

    Update Empty.jsx

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -5,7 +5,7 @@ function Empty() {
   return <div className="border-conversation-border border-l w-full
      bg-panel-header-background flex flex-col h-[100vh] border-b-4
    border-b-icon-green items-center justify-center">
-<Image src="/Whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+<Image src="/whatsapp.gif" alt="Whatsapp" height={300} width={300} />
   </div>;
 }
 
