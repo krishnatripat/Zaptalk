a564474553730f53ad3ddd1a1b262a376ee3489c
krishna tripathi <154440810+krishnatripat@users.noreply.github.com>
2025-06-03 18:31:05 +0530

    Update login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -57,7 +57,8 @@ function login() {
 
   return <> <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
     <div className="flex items-center justify-center text-white  gap-2">
-      <Image src="/Whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+      <img src="/Whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+
       <span className="text-7xl"> Zaptalk</span>
     </div>
     <button className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg" onClick={handlelogin}><FcGoogle className="text-4xl" />
